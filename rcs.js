e797dfd16f1553f53a9b52ad0cc8c86cec8ec1d8
Glenn Rusialda <100415361+grusialda@users.noreply.github.com>
2025-01-30 16:35:23 +0800

    Added playRound function

--- rcs.js
+++ rcs.js
@@ -1,4 +1,4 @@
-function getcomputerChoice() {
+function getComputerChoice() {
   if (Math.random() >= 0.3) {
     return "rock";
   } else if (Math.random() >= 0.6) {
@@ -12,9 +12,36 @@ let getHumanChoice = prompt(
   "Rock, Paper, Scissor"
 ).toLowerCase();
 
-// function getHumanChoice() {
-//   return prompt("Enter your choice", "Rock, Paper, Scissor").toLowerCase();
-// }
-
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "scissor") ||
+    (humanChoice === "scissor" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    humanScore++;
+    console.log(
+      `You win!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
+    );
+  } else if (
+    (humanChoice === "rock" && computerChoice === "rock") ||
+    (humanChoice === "scissor" && computerChoice === "scissor") ||
+    (humanChoice === "paper" && computerChoice === "paper")
+  ) {
+    console.log(
+      `Draw!!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
+    );
+  } else {
+    computerScore++;
+    console.log(
+      `You lose!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
+    );
+  }
+}
+const humanSelection = getHumanChoice;
+console.log(humanSelection);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+playRound(humanSelection, computerSelection);
