5343804d854249e755fa0b6a60331b394874a962
Glenn Rusialda <100415361+grusialda@users.noreply.github.com>
2025-01-30 16:39:22 +0800

    Added playGame function

--- rcs.js
+++ rcs.js
@@ -12,36 +12,39 @@ let getHumanChoice = prompt(
   "Rock, Paper, Scissor"
 ).toLowerCase();
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  if (
-    (humanChoice === "rock" && computerChoice === "scissor") ||
-    (humanChoice === "scissor" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock")
-  ) {
-    humanScore++;
-    console.log(
-      `You win!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
-    );
-  } else if (
-    (humanChoice === "rock" && computerChoice === "rock") ||
-    (humanChoice === "scissor" && computerChoice === "scissor") ||
-    (humanChoice === "paper" && computerChoice === "paper")
-  ) {
-    console.log(
-      `Draw!!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
-    );
-  } else {
-    computerScore++;
-    console.log(
-      `You lose!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
-    );
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice === "rock" && computerChoice === "scissor") ||
+      (humanChoice === "scissor" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+      humanScore++;
+      console.log(
+        `You win!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
+      );
+    } else if (
+      (humanChoice === "rock" && computerChoice === "rock") ||
+      (humanChoice === "scissor" && computerChoice === "scissor") ||
+      (humanChoice === "paper" && computerChoice === "paper")
+    ) {
+      console.log(
+        `Draw!!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
+      );
+    } else {
+      computerScore++;
+      console.log(
+        `You lose!!.. Your score is ${humanScore}. Computer score is ${computerScore}`
+      );
+    }
   }
+  const humanSelection = getHumanChoice;
+  console.log(humanSelection);
+  const computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  playRound(humanSelection, computerSelection);
 }
-const humanSelection = getHumanChoice;
-console.log(humanSelection);
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-playRound(humanSelection, computerSelection);
+playGame();
